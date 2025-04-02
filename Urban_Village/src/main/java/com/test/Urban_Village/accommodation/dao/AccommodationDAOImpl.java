1a91e3efd85b9f4f2a0e216e643b2edf01e66cfd
HanaSeo <gkskquf123123@gmail.com>
2025-04-02 10:16:59 +0900

    뼈대2완료

--- Urban_Village/src/main/java/com/test/Urban_Village/accommodation/dao/AccommodationDAOImpl.java
+++ Urban_Village/src/main/java/com/test/Urban_Village/accommodation/dao/AccommodationDAOImpl.java
@@ -1,5 +1,11 @@
 package com.test.Urban_Village.accommodation.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class AccommodationDAOImpl implements AccommodationDAO {
+@Autowired SqlSession session;
 
 }
--- Urban_Village/src/main/java/com/test/Urban_Village/accommodation/service/AccommodationServiceImpl.java
+++ Urban_Village/src/main/java/com/test/Urban_Village/accommodation/service/AccommodationServiceImpl.java
@@ -3,7 +3,6 @@ package com.test.Urban_Village.accommodation.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.test.Urban_Village.accommodation.dao.AccommodationDAO;
 import com.test.Urban_Village.accommodation.dao.AccommodationDAOImpl;
 
 @Service
